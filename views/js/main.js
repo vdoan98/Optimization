c730c4246fcf5d310c6e247dc24b66da0ea1f355
Vy Doan <vdoan98@gmail.com>
2017-06-26 16:42:27 -0500

    490 & 493 Commented out

--- views/js/main.js
+++ views/js/main.js
@@ -487,17 +487,18 @@ console.log("Time to generate pizzas on load: " + timeToGenerate[0].duration + "
 
 // Iterator for number of times the pizzas in the background have scrolled.
 // Used by updatePositions() to decide when to log the average time per frame
-var frame = 0;
+// var frame = 0;
 
 // Logs the average amount of time per 10 frames needed to move the sliding background pizzas on scroll.
-function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
-  var numberOfEntries = times.length;
-  var sum = 0;
-  for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
-    sum = sum + times[i].duration;
-  }
-  console.log("Average scripting time to generate last 10 frames: " + sum / 10 + "ms");
-}
+// times is the array of User Timing measurements from updatePositions()
+// function logAverageFrame(times) {   
+//   var numberOfEntries = times.length;
+//   var sum = 0;
+//   for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
+//     sum = sum + times[i].duration;
+//   }
+//   console.log("Average scripting time to generate last 10 frames: " + sum / 10 + "ms");
+// }
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
